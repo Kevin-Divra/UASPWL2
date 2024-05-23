0ed081dc91d0374eb30057b8f770f07c29982529
bambangsangdewa <bambangaja2004@gmail.com>
2024-05-23 16:57:57 +0700

    api babyneeds

--- app/Http/Controllers/API/BabyneedsController.php
+++ app/Http/Controllers/API/BabyneedsController.php
@@ -7,87 +7,242 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use App\Models\Babyneed;
+use OpenApi\Annotations as OA;
 
+/**
+ * Class Babyneed.
+ * 
+ * @author Bambang <bambang.422023005@civitas.ukrida.ac.id>
+ */
 class BabyneedsController extends Controller
 {
    /**
-    * Display a listing of the item.
-    *
-    * @return \Illuminate\Http\Response
-     */ 
+    * @OA\Get(
+    *   path="/api/babyneed",
+    *   tags={"Babyneeds"},
+    *   summary="Display a listing of items",
+    *  operationId="index",
+    *   @OA\Response(
+    *       response=200,
+    *       description="seccessful",
+    *       @OA\JsonContent()
+    *     )
+    * )
+    */
     public function index()
     {
         return Babyneed::get();
     }
-
+    
     /**
-     * Store a newly created item in storage.
+     * @OA\Post(
+     *      path="/api/babyneed",
+     *      tags={"Babyneeds"},
+     *      summary="Store a newly created item",
+     *      operationId="store",
+     *       @OA\Response(
+     *          response=400,
+     *          description="invalid input",
+     *          @OA\JsonContent()
+     *      ),
+     *       @OA\Response(
+     *          response=201,
+     *          description="successful",
+     *          @OA\JsonContent()
+     *      ),
      * 
-     * @param \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response            
+     *      @OA\RequestBody(
+     *          required=true,
+     *          description="Request body description",
+     *          @OA\JsonContent(
+     *              ref="#/components/schemas/Babyneed",
+     *              example={"Nama": "T SHIRT AND DENIM JOGGERS SET", "ukuran": "6 bulan",
+     *                      "image": "https://freepngimg.com/png/53976-baby-clothes-download-hq-png",
+     *                      "description" : "baju ini sangat nyaman dan tipis tidak membuat bayi keringatan.",
+     *                      "price" : 50000
+     *              }
+     *          ),
+     *      )   
+     * )         
      */
-    public function store(Request $request)
-    {
-        try{
-        $babyneedss = new Babyneed;
-        $babyneedss->fill($request->validated())->save();
-        
-        return $babyneedss;
+     public function store(Request $request)
+ {
+     try {
+         $validator = Validator::make($request->all(), [
+            'nama' => 'required',
+         ]);
+         if ($validator->fails()) {
+            throw new HttpException(400, $validator->messages()->first());
+         }
+         $babyneedss = new Babyneed;
+         $babyneedss->fill($request->all())->save();
+         return $babyneedss;
 
-    }catch(\Exception $exception){
-        throw new HttpException(400, "Invalid data : {$exception->getMessage}");
-        }
-    }
+     }catch(\Exception $exception){
+        throw new HttpException(400, "Invalid data : {$exception->getMessage()}");
+     }
+ }
 
     /**
-     * Display the specified item.
-     * 
-     * @param int $id
-     * @return \Illuminate\Http\Response
+     * @OA\Get(
+     *      path="/api/babyneed/{id}",
+     *      tags={"Babyneeds"},
+     *      summary="Display the specified item",
+     *      operationId="show",
+     *      @OA\Response(
+     *          response=404,
+     *          description="Item not found",
+     *          @OA\JsonContent()
+     *      ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="invalid input",
+     *          @OA\JsonContent()
+     *      ),
+     *  @OA\Response(
+     *          response=200,
+     *          description="Successful",
+     *          @OA\JsonContent()
+     *       ),
+     *  @OA\Parameter(
+     *      name="id",
+     *      in="path",
+     *      description="ID of item that needs to be displayed",
+     *      required=true,
+     *      @OA\Schema(
+     *          type="integer",
+     *          format="int64"
+     *          )
+     *      ),
+     *     
+     *   )
      */
     public function show($id)
     {
-        $babyneedss = Babyneed::findOrfail($id);
+        $babyneedss = Babyneed::find($id);
+        if(!$babyneedss){
+            throw new HttpException(404, 'item not found');
+        }
 
         return $babyneedss;
     }
 
     /**
-     * Update the specified item in storage.
-     * 
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     *  @return \Illuminate\Http\Response
-     */
+ * @OA\Put(
+ *      path="/api/babyneed/{id}",
+ *      tags={"Babyneeds"},
+ *      summary="Update the specified item",
+ *      operationId="update",
+ *      @OA\Response(
+ *          response=404,
+ *          description="item not found",
+ *          @OA\JsonContent()
+ *      ),
+ *      @OA\Response(
+ *          response=400,
+ *          description="invalid input",
+ *          @OA\JsonContent()
+ *      ),
+ *      @OA\Response(
+ *          response=200,
+ *          description="successful",
+ *          @OA\JsonContent()
+ *      ),
+ *      @OA\Parameter(
+ *          name="id",
+ *          in="path",
+ *          description="ID item that needs to be updated",
+ *          required=true,
+ *          @OA\Schema(
+ *              type="integer",
+ *              format="int64"
+ *          )
+ *      ),
+ *      @OA\RequestBody(
+ *          required=true,
+ *          description="Request body description",
+ *          @OA\JsonContent(
+ *              ref="#/components/schemas/Babyneed",
+ *              example={
+ *                  "Nama": "T SHIRT AND DENIM JOGGERS SET",
+ *                  "ukuran": "6 bulan",
+ *                  "image": "https://freepngimg.com/png/53976-baby-clothes-download-hq-png",
+ *                  "description": "baju ini sangat nyaman dan tipis tidak membuat bayi keringatan",
+ *                  "price": 50000
+ *              }
+ *          )
+ *      )
+ * )
+ */
     public function update(Request $request, $id)
     {
-        if (!$id) {
-            throw new HttpException(400, "invalid id");
+        $babyneedss = Babyneed::find($id);
+        if (!$babyneedss) {
+            throw new HttpException(400, "item not found");
         }
 
         try {
-            $babyneedss = Babyneed::find($id);
-            $babyneedss->fill($request->validated())->save();
-
+           $validator = Validator::make($request->all(), [
+                'nama' => 'required',
+           ]);
+           if ($validator->fails()){
+                throw new HttpException(400, $validator->messages()->first());
+           }
+         $babyneedss->fill($request->all())->save();
+         return response()->json(array('message'=>'Updated sucessfully'), 200);
             return $babyneedss;
 
         } catch(\Exception $exception){
-            throw new HttpException(400, "Invalid data - {$exception->getmessage}");
-        }
-       
+            throw new HttpException(400, "Invalid data : {$exception->getmessage()}");
+        }  
     }
 
-    /**
-     * Remove the specified item from storage.
-     * 
-     *  @param int $id
-     *  @return \Illuminate\Http\Response
-     */
+   /**
+    * @OA\Delete(
+    *     path="/api/babyneed/{id}",
+    *     tags={"Babyneeds"},
+    *     summary="Remove the specified item",
+    *     operationId="destroy",
+    *      @OA\Response(
+     *          response=404,
+     *          description="item not found",
+     *          @OA\JsonContent()
+     *      ),
+     *       @OA\Response(
+     *          response=400,
+     *          description="invalid input",
+     *          @OA\JsonContent()
+     *      ),
+     *       @OA\Response(
+     *          response=200,
+     *          description="Successful",
+     *          @OA\JsonContent()
+     *      ),
+     *      @OA\Parameter(
+     *          name="id",
+     *          in="path",
+     *          description="ID item that needs to be updated",
+     *          required=true,
+     *          @OA\Schema(
+     *              type="integer",
+     *              format="int64"
+     *          )
+     *      ),
+    *   )
+    */
     public function destroy($id)
     {
-        $babyneedss = Babyneed::findOrfail($id);
-        $babyneedss->delete();
+        $babyneedss = Babyneed::find($id);
+       if(!$babyneedss){
+            throw new httpException(404, 'Item not found');
+       }
 
-        return response()->json(null, 204);
+       try {
+            $babyneedss->delete();
+            return response()->json(array('message'=>'Deleted successfully'), 200);
+      
+        } catch(\Exception $exception){
+            throw new HttpException(400, "invalid data : {$exception->getMessage()}");
+        }   
     }
 }
--- app/Models/Babyneed.php
+++ app/Models/Babyneed.php
@@ -8,6 +8,21 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
 use OpenApi\Annotations as OA;
 
+
+/**
+ * class Babyneed.  
+ * 
+ * @author Bambang <bambang.422023005@civitas.ukrida.ac.id>
+ * 
+ * @OA\Schema(
+ *     description="Babyneed model",
+ *     title="Babyneed model",
+ *     required={"nama"},
+ *     @OA\Xml(
+ *          name="Babyneed"
+ *      )
+ * )
+ */
 class Babyneed extends Model
 {
     //use HasFactory;
@@ -15,9 +30,11 @@ class Babyneed extends Model
     protected $table = 'babyneeds';
     protected $fillable = [
         'id',
-        'nama_item_baby',
+        'nama',
         'ukuran',
         'price',
+        'description',
+        'image',
         'created_at',
         'created_by',
         'updated_at',
